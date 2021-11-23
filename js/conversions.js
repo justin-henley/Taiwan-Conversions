f2e364426bd12b03ccfeb870c6cc703f6c103c2a
justin-henley <justin.a.henley@gmail.com>
2021-11-24 02:54:44 +0800

    currently converts in one direction only

--- js/conversions.js
+++ js/conversions.js
@@ -8,6 +8,40 @@ export const sayHello = () => {
  * @param {string} toUnit - A name representing the unit to be converted to
  * @return {number} - The measure converted to the chosen unit, or undefined
  */
-export const convertUnit = (fromUnit, measure, toUnit) => {
-  return undefined;
+export const convertArea = (measure, fromUnit, toUnit) => {
+  const taiwaneseToSqMeters = {
+    ping: 400 / 121,
+    mu: 12000 / 121,
+    fen: 117360 / 121,
+    jia: 1173600 / 121,
+    li: 5868000 / 121,
+  };
+  const sqMetersToForeign = {
+    sqmeter: 1,
+    sqfoot: 1 / 0.09290341,
+    acre: 1 / 4046.873,
+    hectare: 1 / 10000,
+  };
+
+  return measure * taiwaneseToSqMeters[fromUnit] * sqMetersToForeign[toUnit];
 };
+
+export function convertMass(measure, fromUnit, toUnit) {
+  const taiwaneseToKg = {
+    li: 3 / 80000,
+    fen: 3 / 8000,
+    qian: 3 / 800,
+    liang: 3 / 80,
+    jin: 3 / 5,
+    dan: 60,
+  };
+
+  const kgToForeign = {
+    ounce: 1 / 0.028349523125,
+    pound: 1 / 0.45359237,
+    ton: 1 / 907.18474,
+    metricTon: 1 / 1000,
+  };
+
+  return measure * taiwaneseToKg[fromUnit] * kgToForeign[toUnit];
+}
--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
 import * as Conversions from "./conversions.js";
 
 Conversions.sayHello();
+
+const pingToSqMeter = (ping) => {
+  let sqmeter = (ping * 400.0) / 121.0;
+  return sqmeter;
+};
+
+// Launch app
+document.addEventListener("readystatechange", (event) => {
+  if (event.target.readyState === "complete") {
+    initApp();
+  }
+});
+
+const initApp = () => {
+  const conversionTypeSelect = document.getElementById("conversionType");
+  conversionTypeSelect.addEventListener("change", (event) => {
+    console.log(event.target.value);
+  });
+
+  /* // Add listener to convert button
+    const convertButton = document.getElementById('convert');
+    convertButton.addEventListener("submit", event => {
+        event.preventDefault();
+        processSubmission();
+    }) */
+};
+
+const testConvert = (measure, from, to) => {
+  console.log(Conversions.convertArea(measure, from, to));
+};
+
+testConvert(10, "ping", "sqfoot");
